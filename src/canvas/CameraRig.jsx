0c05ae393c3d361c90f98dfb0c38494477c7e603
calebbenjin <calebben94@gmail.com>
2023-04-06 08:11:50 +0100

    feat: created the CameraRig and Shirt component

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -5,20 +5,38 @@ import { useSnapshot } from 'valtio'
 
 import state from '../store'
 
-const CameraRig = ({ children}) => {
+const CameraRig = ({ children }) => {
   const group = useRef()
   const snap = useSnapshot(state)
 
-  useFrame(() => {
+  useFrame((state, delta) => {
+    // Responsiveness
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+
+    // Set the inital positon of the model
+    let targetPosition = [-0.4, 0, 2];
+    if(snap.intro) {
+      if(isBreakpoint) targetPosition = [0, 0, 2];
+      if(isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if(isMobile) targetPosition = [0, 0, 2.5]
+      else targetPosition = [0, 0, 2]
+    }
+
+    // set model camera position
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+
     //  Set the model rotation smoothly
-    easeIn.dampE(
+    easing.dampE(
       group.current.rotation,
-      [state.pointer.y / 10, -state.pointer.x/ 5, 0 ],
+      [state.pointer.y / 10, -state.pointer.x / 5, 0 ],
       0.25,
+      delta
     )
   })
   return (
-    <group>
+    <group ref={group}>
       {children}
     </group>
   )
