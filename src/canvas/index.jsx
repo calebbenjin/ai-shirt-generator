f3eb2fa1b08a10535657ae70f0970d291f2b85a3
calebbenjin <calebben94@gmail.com>
2023-04-06 08:24:01 +0100

    feat: Created Backdrop compoonent

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -8,7 +8,11 @@ import CameraRig from './CameraRig'
 
 const CanvasModel = () => {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 0], fov: 25}}>
+    <Canvas 
+    shadows 
+    camera={{ position: [0, 0, 0], fov: 25}}
+    gl={{ preserveDrawingBuffer: true}}
+    className="w-full max-w-full h-full transition-all ease-in">
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
       <CameraRig>
