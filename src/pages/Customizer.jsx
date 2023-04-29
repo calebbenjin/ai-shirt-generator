840ec215eacee6200bb1d99a6422f38ac065ec7b
calebbenjin <calebben94@gmail.com>
2023-04-29 01:40:50 +0100

    feat: Tab component and states for Generating Tab COntent

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -12,6 +12,22 @@ import { Tab, AIPicker, ColorPicker, CustomButton, FilePicker } from '../compone
 
 const Customizer = () => {
   const snap = useSnapshot(state)
+
+  const [file, setFile] = useState('');
+
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  //  show tab content depending on the activeTab
+  const generateTabContent = () => {}
+
+  
   return (
     <AnimatePresence>
       {!snap.intro && (
